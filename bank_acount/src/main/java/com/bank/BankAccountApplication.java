aa8066b1756602a7b34932d98a479f918f93357c
OsaidM <osaid40@hotmail.com>
2025-08-04 19:07:11 +0300

    adding new account

--- bank_acount/src/main/java/com/bank/BankAccountApplication.java
+++ bank_acount/src/main/java/com/bank/BankAccountApplication.java
@@ -18,6 +18,7 @@ public class BankAccountApplication {
         BankAccount Wesam = new BankAccount(750, 220);
         BankAccount Ahmad = new BankAccount(550, 150);
         BankAccount Omar = new BankAccount(400, 70);
+        BankAccount Ola = new BankAccount(400, 70);
         
         
         Osaid.withdraw(500, 1);
