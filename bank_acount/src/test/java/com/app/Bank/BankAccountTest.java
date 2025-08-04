13edc5b885779d67c4b4f5007b5dcd36821b73ba
OsaidM <osaid40@hotmail.com>
2025-08-04 18:56:18 +0300

    re-enhance for the whole architecture

---
+++ bank_acount/src/test/java/com/app/Bank/BankAccountTest.java
@@ -0,0 +1,24 @@
+package com.app.Bank;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+class BankAccountTest {
+
+	@Before
+	void setUp() throws Exception {
+	}
+
+	@After
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testBank() {
+		assertEquals(true, true);
+	}
+
+}
